ef5d2e2ab078948c1ecfd15e75bb3a54c5a7c99c
Heitor Tremura <30376296+htremura@users.noreply.github.com>
2022-06-16 11:15:41 -0400

    support multi line in a janky way

--- minesweep.py
+++ minesweep.py
@@ -2,24 +2,29 @@ import pyperclip
 import string
 
 jazzystring = ""
+notdone = True
 
-boring = input("Gimme text to turn into a minesweeper for Discord!\n").upper()
-for letter in range(len(boring)):
-    if boring[letter] == "1":
-        jazzystring += "||:one:||"
-    elif boring[letter] == "2":
-        jazzystring += "||:two:||"
-    elif boring[letter] == "3":
-        jazzystring += "||:three:||"
-    elif boring[letter] == "4":
-        jazzystring += "||:four:||"
-    elif boring[letter] == "5":
-        jazzystring += "||:five:||"
-    elif boring[letter] == " ":
-        jazzystring += "||:white_large_square:||"
-    elif boring[letter] == "💥":
-        jazzystring += "||:bomb:||"
-    elif boring[letter] == "\n":
-        jazzystring += "\n"
+while notdone:
+    boring = input("Gimme the first line to turn into a minesweeper for Discord!\n").upper()
+    for letter in range(len(boring)):
+        if boring[letter] == "1":
+            jazzystring += "||:one:||"
+        elif boring[letter] == "2":
+            jazzystring += "||:two:||"
+        elif boring[letter] == "3":
+            jazzystring += "||:three:||"
+        elif boring[letter] == "4":
+            jazzystring += "||:four:||"
+        elif boring[letter] == "5":
+            jazzystring += "||:five:||"
+        elif boring[letter] == " ":
+            jazzystring += "||:white_large_square:||"
+        elif boring[letter] == "💥":
+            jazzystring += "||:bomb:||"
+    jazzystring += "\n"
+    print("We got: " + jazzystring)
+    maybe = input("Are we done?\n").upper()
+    if (maybe == "Y") || (maybe == "YES"):
+        notdone = False
 
 pyperclip.copy(jazzystring)
