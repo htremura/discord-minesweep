6d7cf1878b9a99e7dabefa6f126ba2e215d8690b
Heitor Tremura <30376296+htremura@users.noreply.github.com>
2022-06-16 10:52:06 -0400

    rename main and update for beginning of letters

---
+++ minesweep.py
@@ -0,0 +1,28 @@
+import pyperclip
+import string
+
+jazzystring = ""
+
+boring = input("Gimme text to turn into a minesweeper for Discord!\n").upper()
+for letter in range(len(boring)):
+    if boring[letter] == "1":
+        jazzystring += "||:one:||"
+    elif boring[letter] == "2":
+        jazzystring += "||:two:||"
+    elif boring[letter] == "3":
+        jazzystring += "||:three:||"
+    elif boring[letter] == "4":
+        jazzystring += "||:four:||"
+    elif boring[letter] == "5":
+        jazzystring += "||:five:||"
+    elif boring[letter] == "	":
+        jazzystring += "||:white_large_square:||"
+    else:
+        jazzystring += "\n"
+
+pyperclip.copy(jazzystring)
+
+1	1	1	 	 	 
+1	💥	2	1	1	 
+1	2	3	💥	2	1
+ 	1	💥	2	2	💥
