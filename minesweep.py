8c564167e3fe53425199f4d8342c64bb44611cff
Heitor Tremura <30376296+htremura@users.noreply.github.com>
2022-06-17 08:04:59 -0400

    correct letter count and print at the end

--- minesweep.py
+++ minesweep.py
@@ -17,67 +17,77 @@ while notdone:
                 break
             jazzystring += "||:one:||"
             i+=6+3
+            jazzystring += "\n"
         elif boring[letter] == "2":
             if (i+6+3>2000):
                 print("Too many characters!")
                 break
             jazzystring += "||:two:||"
             i+=6+3
+            jazzystring += "\n"
         elif boring[letter] == "3":
             if (i+6+5>2000):
                 print("Too many characters!")
                 break
             jazzystring += "||:three:||"
             i+=6+5
+            jazzystring += "\n"
         elif boring[letter] == "4":
             if (i+6+4>2000):
                 print("Too many characters!")
                 break
             jazzystring += "||:four:||"
             i+=6+4
+            jazzystring += "\n"
         elif boring[letter] == "5":
             if (i+6+4>2000):
                 print("Too many characters!")
                 break
             jazzystring += "||:five:||"
             i+=6+4
+            jazzystring += "\n"
         elif boring[letter] == "6":
             if (i+6+3>2000):
                 print("Too many characters!")
                 break
             jazzystring += "||:six:||"
             i+=6+3
+            jazzystring += "\n"
         elif boring[letter] == "7":
             if (i+6+5>2000):
                 print("Too many characters!")
                 break
             jazzystring += "||:seven:||"
             i+=6+5
+            jazzystring += "\n"
         elif boring[letter] == "8":
             if (i+6+5>2000):
                 print("Too many characters!")
                 break
             jazzystring += "||:eight:||"
             i+=6+5
+            jazzystring += "\n"
         elif boring[letter] == "9":
             if (i+6+4>2000):
                 print("Too many characters!")
                 break
             jazzystring += "||:nine:||"
             i+=6+4
+            jazzystring += "\n"
         elif boring[letter] == " ":
             if (i+6+18>2000):
                 print("Too many characters!")
                 break
             jazzystring += "||:white_large_square:||"
             i+=6+18
+            jazzystring += "\n"
         elif boring[letter] == "💥":
             if (i+6+4>2000):
                 print("Too many characters!")
                 break
             jazzystring += "||:bomb:||"
             i+=6+4
-    jazzystring += "\n"
-    i+=1
+            jazzystring += "\n"
 
+print("The letter count is: " + i)
 pyperclip.copy(jazzystring)
