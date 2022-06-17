39a7337c1bd30bbb6b1e6bcedc6ec814d2198060
Heitor Tremura <30376296+htremura@users.noreply.github.com>
2022-06-17 08:20:44 -0400

    added toolong flag

--- minesweep.py
+++ minesweep.py
@@ -4,6 +4,7 @@ import string
 jazzystring = ""
 notdone = True
 pasteloop = True
+toolong = False
 i = 0
 
 while notdone:
@@ -14,79 +15,79 @@ while notdone:
     for letter in range(len(boring)):
         if boring[letter] == "1":
             if (i+6+3>2000):
-                i+=6+3
+                toolong = True
                 break
             jazzystring += "||:one:||"
             i+=6+3
         elif boring[letter] == "2":
             if (i+6+3>2000):
-                i+=6+3
+                toolong = True
                 break
             jazzystring += "||:two:||"
             i+=6+3
         elif boring[letter] == "3":
             if (i+6+5>2000):
-                i+=6+5
+                toolong = True
                 break
             jazzystring += "||:three:||"
             i+=6+5
         elif boring[letter] == "4":
             if (i+6+4>2000):
-                i+=6+4
+                toolong = True
                 break
             jazzystring += "||:four:||"
             i+=6+4
         elif boring[letter] == "5":
             if (i+6+4>2000):
-                i+=6+4
+                toolong = True
                 break
             jazzystring += "||:five:||"
             i+=6+4
         elif boring[letter] == "6":
             if (i+6+3>2000):
-                i+=6+3
+                toolong = True
                 break
             jazzystring += "||:six:||"
             i+=6+3
         elif boring[letter] == "7":
             if (i+6+5>2000):
-                i+=6+5
+                toolong = True
                 break
             jazzystring += "||:seven:||"
             i+=6+5
         elif boring[letter] == "8":
             if (i+6+5>2000):
-                i+=6+5
+                toolong = True
                 break
             jazzystring += "||:eight:||"
             i+=6+5
         elif boring[letter] == "9":
             if (i+6+4>2000):
-                i+=6+4
+                toolong = True
                 break
             jazzystring += "||:nine:||"
             i+=6+4
         elif boring[letter] == " ":
             if (i+6+18>2000):
-                i+=6+18
+                toolong = True
                 break
             jazzystring += "||:white_large_square:||"
             i+=6+18
         elif boring[letter] == "💥":
             if (i+6+4>2000):
-                i+=6+4
+                toolong = True
                 break
             jazzystring += "||:bomb:||"
             i+=6+4
-    if not (i>2000):
+    if not toolong:
         jazzystring += "\n"
     else:
         print("Too many characters!")
         break
 
-while pasteloop == True:
-    loopq = input("Are we done with the pasteloop?").upper()
+while toolong:
+    loopq = input("Are we done with the pasteloop? (y/n)").upper()
     if loopq == "Y":
-        pasteloop = False
+        toolong = False
 print("The letter count is: " + str(i))
 pyperclip.copy(jazzystring)
